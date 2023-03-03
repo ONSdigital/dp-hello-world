77f5437dd0aa10a4ab2b0bf167738366a8649108
Nathan Shumoogum <nathan.shumoogum@methods.co.uk>
2023-03-03 15:55:41 +0000

    Test package name renamed to prevent recursion issues

--- dp-hello-world-library/hello_world.go
+++ dp-hello-world-library/hello_world.go
@@ -1,3 +1,4 @@
+// TODO replace helloworld package
 package helloworld
 
 type HelloWorld struct {
--- dp-hello-world-library/hello_world_test.go
+++ dp-hello-world-library/hello_world_test.go
@@ -1,19 +1,23 @@
-package helloworld
+package helloworld_test
 
 import (
 	"testing"
 
+	hc "github.com/ONSdigital/dp-hello-world-library"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+// TODO replace helloworld tests
 func TestNewHelloWorld(t *testing.T) {
 	Convey("Given a new hello world message", t, func() {
 		message := "hello new world!"
+
 		Convey("When NewHelloWorld func is called", func() {
-			helloWorld := NewHelloWorld(message)
+			helloWorld := hc.NewHelloWorld(message)
+
 			Convey("Then HelloWorld object is returned containing new message", func() {
 				So(helloWorld, ShouldNotBeNil)
-				So(helloWorld, ShouldResemble, &HelloWorld{Message: message})
+				So(helloWorld, ShouldResemble, &hc.HelloWorld{Message: message})
 			})
 		})
 	})
